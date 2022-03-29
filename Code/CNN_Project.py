05da607f31c5c5db3efc560004e65dafb32c2343
bhagattirth <76080774+bhagattirth@users.noreply.github.com>
2022-03-29 15:03:22 -0400

    Added find average imageheight

--- Code/CNN_Project.py
+++ Code/CNN_Project.py
@@ -16,6 +16,21 @@ import random
 from tqdm import tqdm
 from PIL import Image #not sure if this one is necessary
 
+def Find_Average_Image_Dimension():
+    width = 0
+    height = 0
+    number_Of_Images = 0
+    directory = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "thecarconnectionpicturedataset"))
+    for file in tqdm(os.listdir(directory)):
+        filepath = os.path.join(directory,file)
+        img = Image.open(filepath) # Opens the Image
+        number_Of_Images +=1
+        width += img.size[0]
+        height += img.size[1]
+    width = width/number_Of_Images
+    height = height/ number_Of_Images
+    return (width,height)
+
 # Loads the dataset from the file location
 def load_dataset(crop_width, crop_height, batch_size = 32, train=True):
     dataset = []
@@ -58,17 +73,23 @@ def load_dataset(crop_width, crop_height, batch_size = 32, train=True):
 
     return np.array(dataset)
 
+
+
+
 def plot_image(image, crop_width, crop_height):
     image = image.reshape(-1,crop_width,crop_height,3)
     plt.imshow(image[0])
     plt.show()
     return
 
+#Since I found the average image size there is no need to run this function anymore
+#print(Find_Average_Image_Dimension())
 
-crop_width = 200 # Width of the cropped image
-crop_height = 200 # Height of the cropped image
 
-dataset = load_dataset(crop_width,crop_height,batch_size=32,train=True)
-ex_image = dataset[random.randint(0,10)]
+crop_width = 320  # Average Width of the cropped image is 320
+crop_height = 300  # Average Height of the cropped image is 230
+dataset = load_dataset(crop_width, crop_height, batch_size=32, train=True)
+
+ex_image = dataset[random.randint(0, 10)]
 print("image shape:", ex_image.shape)
 plot_image(ex_image,crop_width=crop_width,crop_height=crop_height)
