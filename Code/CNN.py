a8f2ec626a786309c65fb1d07ee86bcb00cdf8c4
bhagattirth <76080774+bhagattirth@users.noreply.github.com>
2022-03-29 18:25:21 -0400

    Update CNN.py

--- Code/CNN.py
+++ Code/CNN.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
+        #the inital image shape is (3, 310, 210)
         self.conv1 = nn.Conv2d(3, 6, (5,5))
         self.maxpool = torch.nn.MaxPool2d((2,2), stride=(2,2))
         self.linear = nn.linear()
