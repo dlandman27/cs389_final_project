b1e7ef74592865476afed49231a81bd69439a100
Dylan Landman <dylan@landmans.us>
2022-04-04 18:16:44 -0400

    Fixed Tirths Errors

--- Code/CNN_Project.py
+++ Code/CNN_Project.py
@@ -1,3 +1,4 @@
+from operator import xor
 import time
 import torch
 import torch.nn as nn
@@ -19,6 +20,7 @@ from CNN import CNN
 from sklearn.model_selection import train_test_split
 
 
+# Finds the average width and height of a givenn set of images
 def Find_Average_Image_Dimension():
     width = 0
     height = 0
@@ -40,19 +42,21 @@ def Find_Average_Image_Dimension():
 
 def load_dataset( crop_width, crop_height, batch_size = 32, train=True):
 
-    training_set_final = []
-    test_set_final = []
-    training_feature_set = []
-    test_feature_set = []
-    dataset = []
+    training_set_final = [] # This is the final training set
+    test_set_final = [] # This is the final test set
+    training_feature_set = [] # This is the final training feature set
+    test_feature_set = [] # This is the final test feature set
+    dataset = [] # This is an array of all the image URLs
 
     directory = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "thecarconnectionpicturedataset"))
     for file in tqdm(os.listdir(directory)):
         filepath = os.path.join(directory, file)
         dataset.append(filepath)
 
+    # Splits the dataset into training and test sets
     training_Set, test_set = train_test_split(dataset, test_size=0.3, random_state=25)
 
+    # Crops the image and adds it to the feature set
     def process_Image_Set(data, final_set, final_feature):
         counter = 0
         batch_counter = 0
@@ -75,14 +79,16 @@ def load_dataset( crop_width, crop_height, batch_size = 32, train=True):
             img = np.asarray(img).reshape(3, 300, 210)
 
             img_features = np.array(file.split("_"))  # Gets the features from the file name
+            price = [int(img_features[3])] # Gets the price from the file name
 
+            # TODO DELETE THIS
             if counter > 1000:
                 break
             else:
                 counter += 1
             if batch_counter < batch_size:
                 batch.append(img)
-                feature_batch.append(img_features)
+                feature_batch.append(price)
                 batch_counter = batch_counter + 1
             else:
                 final_set.append(np.array(batch))
@@ -120,40 +126,44 @@ ex_image = dataset[random.randint(0, 10)]  # prints an random image from the dat
 plot_image(ex_image, crop_width=crop_width, crop_height=crop_height)
 
 
-
-
 def training(model, dataset, featureSet, loss_function, optimizer, n_epochs, update_interval):
     losses = []
-    count = 0
     for n in range(n_epochs):
-        for i in enumerate(tqdm(dataset)):
-
+        iterator = 0
+        count = 0
+        for i in (tqdm(dataset)):
             optimizer.zero_grad()
-            print(i.shape)
+            
             my_output = model(i)
+
             # print(torch.from_numpy(featureSet[count]).float().shape)
             loss = loss_function(my_output, torch.from_numpy(featureSet[count]).float())
             loss.backward()
             optimizer.step()
             count = count + 1
 
-            if i % update_interval == 0:
+            if iterator % update_interval == 0:
                 losses.append(round(loss.item(), 2))
+            iterator+=1
 
     return model, losses
 
 
-r = training(model, dataset, dataset_features, loss_function, optimizer, n_epochs, update_interval)
 
 
 #Option1:
 #Also variable methods when working with GPU, would look into that
 
 
-
+if  not loadModel:
+    r = training(model, dataset, dataset_features, loss_function, optimizer, n_epochs, update_interval)
 # direct = os.path.realpath(os.path.join(os.path.dirname("ModelState.pt"), "..", "SavedStates"))
 #save model
-torch.save(model.state_dict(), "../SavedStates/ModelState.pt")
+if saveModel:
+    direct = os.path.realpath(os.path.join(os.path.dirname("ModelState.pt"), "..", "SavedStates"))
+    torch.save(r[0].state_dict(), direct)
+
+
 #load entire model
 
 #model.eval()
